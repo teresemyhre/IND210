52cebc97551779bbac7fcbe0f70ca3b7e02c1c75
teresemyhre <144899929+teresemyhre@users.noreply.github.com>
2025-05-01 13:06:03 +0200

    Update Prognose_app.py

--- Prognose_app.py
+++ Prognose_app.py
@@ -127,7 +127,7 @@ for uke_salg in forecast_retailer['yhat'][-fremtidig_uker:]:
 
 # === Vis resultat til bruker ===
 st.markdown("### 🧮 Lageranalyse basert på prognose")
-st.info("Obs: Lageranalysen antar at det ikke gjøres nye bestillinger i perioden.")
+st.markdown("**🔍 Obs:** *Lageranalysen antar at det ikke gjøres nye bestillinger i perioden.*", unsafe_allow_html=True)
 st.write(f"📦 Lager hos wholesaler varer i ca. **{wholesale_uker} uker** gitt prognosen.")
 st.write(f"🛒 Lager hos retailer varer i ca. **{retail_uker} uker** gitt prognosen.")
 
