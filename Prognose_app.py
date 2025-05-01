e0c7f8087f1435f915ce45eb207038b7011acefc
teresemyhre <144899929+teresemyhre@users.noreply.github.com>
2025-05-01 18:52:10 +0200

    Update Prognose_app.py

--- Prognose_app.py
+++ Prognose_app.py
@@ -105,10 +105,13 @@ else:
     df_prophet = df[['Year-Week', valgt_kolonne]].copy()
     df_prophet.columns = ['year_week', 'y']
     # df_prophet['ds'] = pd.date_range(start=startdato, periods=len(df_prophet), freq='W')
-    df_prophet['ds'] = pd.to_datetime(df_prophet['year_week'] + "7", format="%Y-%U-%w")
+    df_prophet['ds'] = pd.to_datetime(df_prophet['year_week'] + "7", format="%Y-%m-%d")
 
     # Filtrer datasettet for å kun bruke data fra startdato og fremover
     df_filtered = df_prophet[df_prophet['ds'] >= startdato]
+
+    # Vis det filtrerte datasettet
+    st.write(df_filtered)
 
     model = Prophet(weekly_seasonality=True)
     model.fit(df_filtered[['ds', 'y']])
